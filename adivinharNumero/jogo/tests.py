8dd60ff753a6178a11beb7da42141afd5e9e6709
luizlimads <luizhenrique.limads@gmail.com>
2023-06-10 13:41:50 -0300

    test: add tests for jogo and usuario apps

--- adivinharNumero/jogo/tests.py
+++ adivinharNumero/jogo/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestJogo(TestCase):
+    def setUp(self):
+        self.password = '123456'
+
+        self.username1 = 'usuario1'
+        self.user1 = User.objects.create_user(
+            username=self.username1,
+            password=self.password
+        )
+        self.username2 = 'usuario2'
+        self.user2 = User.objects.create_user(
+            username=self.username2,
+            password=self.password
+        )
+
+    def test_acessa_atrio_usuario1_sem_estar_logado_falha(self):
+        client = Client()
+        response = client.get(f'/usuario/{self.username1}/atrio/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_acessa_atrio_usuario1_logado_com_usuario2_falha(self):
+        client = Client()
+        client.force_login(self.user2)
+        response = client.get(f'/usuario/{self.username1}/atrio/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_acessa_atrio_usuario1_logado_com_usuario1_sucesso(self):
+        client = Client()
+        client.force_login(self.user1)
+        response = client.get(f'/usuario/{self.username1}/atrio/')
+        self.assertEqual(response.status_code, 200) 
+
--- adivinharNumero/usuario/tests.py
+++ adivinharNumero/usuario/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestUsuario(TestCase):
+    def setUp(self):
+        # Cria um usuário de teste
+        self.username = 'usuario1'
+        self.password = '123456'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+    
+    def test_login_acessoGet(self):
+        client = Client()
+        response = client.get('/usuario/login/')
+        self.assertEqual(response.status_code, 200) 
+
+    def test_login_usuario1_sucesso(self):
+        client = Client()
+        response = client.post('/usuario/login/check/', {'apelido': self.username, 'senha': self.password})        
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/{self.username}/atrio/')
+    
+    def test_login_falsoUsuario_falha(self):
+        client = Client()
+        username = 'falsoUsuario'
+        response = client.post('/usuario/check/', {'apelido': username, 'senha': self.password})                
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
