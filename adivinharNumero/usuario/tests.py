6caa2718ec4dfd09774e02eaf6992cd24d1ebf7e
luizlimads <luizhenrique.limads@gmail.com>
2023-06-12 12:28:51 -0300

    bug(usuario.test): modifications to test_login and test_register

--- adivinharNumero/usuario/tests.py
+++ adivinharNumero/usuario/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from util.Messages import Messages as m
+
 """
     Classe para realização de testes automaticos no app usuarios
     Nome de testes seguindo a seguinda logica
@@ -39,23 +40,6 @@ class TestUsuarioViews(TestCase):
         response = client.get('/usuario/login/check/')
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/usuario/login/')
-
-    def test_login_usuarioForaDePadrao_loga_falha(self):
-        client = Client()
-        test_data = [
-            {'apelido': '', '': self.password}, # usuario e senha vazios
-            {'apelido': 'jao', 'senha': self.password}, # usuario menor que 4 letras
-            {'apelido': self.username, 'senha': 'ddd'}, # senha menor que 6 digitos
-            {'apelido': 'Jaos', 'senha': self.password}, # usuario com letra maiuscula
-            {'apelido': 'joão', 'senha': self.password}, # usuario com acento
-            {'apelido': 'joao teste', 'senha': self.password}, # usuario com espaço em branco
-        ]
-        for data in test_data:
-            response = client.post('/usuario/login/check/', data)
-            messages = [m.message for m in response.wsgi_request._messages]
-            self.assertEqual(response.status_code, 302)
-            self.assertRedirects(response, '/usuario/login/')
-            self.assertIn(messages[0], m.ERRO_LOGAR_USUARIO.value)
    
     def test_login_usuario1_loga_sucesso(self):
         client = Client()
@@ -89,23 +73,69 @@ class TestUsuarioViews(TestCase):
         self.assertEqual(len(messages),1)
         self.assertEqual(messages[0],m.SUCESSO_CADASTRAR_USUARIO.value)
 
-    def test_register_usuarioForaDePadrao_registra_falha(self):
+    def test_register_usuarioForaDePadraoUsuarieESenhaEmBranco_registra_falha(self):
+        data = {'apelido': '', '': self.password} # usuario e senha vazios
         client = Client()
-        test_data = [
-            {'apelido': '', '': self.password}, # usuario e senha vazios
-            {'apelido': 'jao', 'senha': self.password}, # usuario menor que 4 letras
-            {'apelido': self.username, 'senha': 'ddd'}, # senha menor que 6 digitos
-            {'apelido': 'Jaos', 'senha': self.password}, # usuario menos com letra maiuscula
-            {'apelido': 'joão', 'senha': self.password}, # usuario com acento
-            {'apelido': 'joao teste', 'senha': self.password}, # usuario com espaço em braco
-        ]
-        for data in test_data:
-            response = client.post('/usuario/register/check/', data)
-            messages = [m.message for m in response.wsgi_request._messages]
-            self.assertEqual(response.status_code, 302)
-            self.assertRedirects(response, f'/usuario/login/')
-            self.assertIn(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
     
+    def test_register_usuarioForaDePadraoUsuarioMenorQue4_registra_falha(self):
+        data = {'apelido': 'jao', 'senha': self.password} # usuario menor que 4 letras
+        client = Client()
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+    
+    def test_register_usuarioForaDePadraoSenhaMenorQue6_registra_falha(self):
+        data = {'apelido': 'userteste', 'senha': 'ddd'}
+        client = Client()
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+    
+    def test_register_usuarioForaDePadraoUsuarioLetraMaiuscula_registra_falha(self):
+        data = {'apelido': 'Jaos', 'senha': self.password}
+        client = Client()
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+
+    def test_register_usuarioForaDePadraoUsuarioComAcentuacao_registra_falha(self):
+        data = {'apelido': 'joão', 'senha': self.password} # usuario com acento
+        client = Client()
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+
+    def test_register_usuarioForaDePadraoUsuarioComEspacos_registra_falha(self):
+        data = {'apelido': 'joao teste', 'senha': self.password} # usuario com espaço em braco
+        client = Client()
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+    
+    def test_register_usuarioForaDePadraoUsuarioComSimbolos_registra_falha(self):
+        data = {'apelido': 'carl@s', 'senha': self.password} # usuario com espaço em braco
+        client = Client()
+        response = client.post('/usuario/register/check/', data)
+        messages = [m.message for m in response.wsgi_request._messages]
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/usuario/login/')
+        self.assertEqual(messages[0], m.ERRO_CADASTRA_USUARIO.value)
+
     def test_register_usuarioExistente_registra_falha(self):
         client = Client()
         response = client.post('/usuario/register/check/', {'apelido': self.username, 'senha': self.password})
